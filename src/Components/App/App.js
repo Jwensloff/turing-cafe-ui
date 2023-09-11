8217704c82aab3e6ef13fdbae760bd0dc07de0ef
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-11 12:05:10 -0600

    feat: Add a Delete request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,39 +2,48 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Form from '../Form/Form';
 import ReservationContainer from '../ReservationContainer/ReservationContainer';
-import { postReservation, fetchReservations } from '../../apiCalls';
+import {
+  postReservation,
+  fetchReservations,
+  deleteReservation,
+} from '../../apiCalls';
 
 function App() {
-  const [ reservations, setReservations ] = useState([]);
+  const [reservations, setReservations] = useState([]);
 
   useEffect(() => {
     fetchReservations()
-    .then((data) => {
-      console.log(data);
-      setReservations(data);
-    }).catch(error => console.log(error))
-    ;
+      .then((data) => {
+        console.log(data);
+        setReservations(data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   function addReservation(newReservation) {
-    setReservations([...reservations, newReservation])
-    postReservation(newReservation)
-    .catch(error => console.log(error))
+    setReservations([...reservations, newReservation]);
+    postReservation(newReservation).catch((error) => console.log(error));
   }
 
   const cancelReservation = (id) => {
-    const filteredReservations = reservations.filter(reservation => reservation.id !== id);
-      setReservations(filteredReservations)
-  }
+    const filteredReservations = reservations.filter(
+      (reservation) => reservation.id !== id
+    );
+    setReservations(filteredReservations);
+    deleteReservation(id);
+  };
 
   return (
     <div className='App'>
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
-        <Form addReservation={addReservation}/>
+        <Form addReservation={addReservation} />
       </div>
       <div className='resy-container'>
-        <ReservationContainer reservations={reservations} cancelReservation={cancelReservation}/>
+        <ReservationContainer
+          reservations={reservations}
+          cancelReservation={cancelReservation}
+        />
       </div>
     </div>
   );
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,13 @@
 function fetchReservations() {
-  return fetch('http://localhost:3001/api/v1/reservations')
-  .then((resp) =>{
-  if(!resp.ok) {
-    throw new Error ('Something went wrong')
-  }
-   return resp.json()
-});
+  return fetch('http://localhost:3001/api/v1/reservations').then((resp) => {
+    if (!resp.ok) {
+      throw new Error('Something went wrong');
+    }
+    return resp.json();
+  });
 }
 
 function postReservation(reservation) {
-
   const { name, date, time, number } = reservation;
   let data = { name, date, time, number };
 
@@ -19,11 +17,27 @@ function postReservation(reservation) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  })
-    .then((resp) => {
-      if(!resp.ok){
-        throw new Error ('We were unable to add your reservation at this time.')
-      } 
-      return resp.json()})
+  }).then((resp) => {
+    if (!resp.ok) {
+      throw new Error('We were unable to add your reservation at this time.');
+    }
+    return resp.json();
+  });
 }
-export { fetchReservations, postReservation };
+
+function deleteReservation(id) {
+  return fetch(`http://localhost:3001/api/v1/reservations/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((resp) => {
+    if (!resp.ok) {
+      throw new Error(
+        'We were unable to remove your reservation at this time.'
+      );
+    }
+    return resp.json();
+  });
+}
+export { fetchReservations, postReservation, deleteReservation };
